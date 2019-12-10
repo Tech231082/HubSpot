4709cd88107d9d63e3bcb0c58854a188245d12b4
Sushila Goyat <sush.rinwa@gmail.com>
2019-12-10 10:18:29 -0700

    ForgotPasswordPage class added

--- HubSpot/src/main/java/com/qa/pages/ForgotPasswordPage.java
+++ HubSpot/src/main/java/com/qa/pages/ForgotPasswordPage.java
@@ -1,5 +1,38 @@
 package com.qa.pages;
 
-public class ForgotPasswordPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.base.BasePage;
+
+public class ForgotPasswordPage extends BasePage{
+	
+	//page elements
+	@FindBy(xpath="//input[@id='email']")
+	WebElement email;
+	
+	@FindBy(xpath="//i18n-string[@data-key='login.requestReset.header']")
+	WebElement resetYourPasswordButton;
+	
+	@FindBy(xpath="//span[contains(text(),'Go back to ')]")
+	WebElement goBackToLoginButton;
+	
+	//class constructor
+	
+	public ForgotPasswordPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//page methods
+	
+	public void resetPassword() {
+		email.sendKeys("anything.something@gmail.com");
+		
+	}
+	
+	public void goBackToLoginPage() {
+		goBackToLoginButton.click();
+	}
 
 }
---
+++ HubSpot/src/test/java/com/qa/testcases/ForgotPasswordPageTest.java
@@ -0,0 +1,55 @@
+package com.qa.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.base.BasePage;
+import com.qa.pages.ForgotPasswordPage;
+import com.qa.pages.LoginPage;
+
+public class ForgotPasswordPageTest extends BasePage{
+	
+	public LoginPage loginPage;
+	public ForgotPasswordPage forgotPasswordPage;
+	
+	
+	public ForgotPasswordPageTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		loginPage=new LoginPage();
+		forgotPasswordPage=new ForgotPasswordPage();
+		loginPage.clickOnForgotMyPasswordLink();
+	}
+	
+	@Test(enabled=false)
+	public void resetPasswordTest() {
+		
+		forgotPasswordPage.resetPassword();
+		driver.findElement(By.xpath("//i18n-string[@data-key='login.requestReset.header']")).submit();
+		String helpText=driver.findElement(By.xpath("//i18n-string[@data-key='login.requestReset.success.header']")).getText();
+		
+		System.out.println(helpText);
+		Assert.assertEquals(helpText, "Help is on the way","Customized message : Something is wrong while resetting the password");
+	}
+	
+	@Test
+	public void goBackToLoginPageTest() {
+		forgotPasswordPage.goBackToLoginPage();
+		String actual=driver.getTitle();
+        //Assert.assertEquals(actual, "HubSpot login");
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
