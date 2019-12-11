dd01690bbdb72e0047fe2b6e1f36c96ee02962bd
Sushila Goyat <sush.rinwa@gmail.com>
2019-12-11 16:46:17 -0700

    ReportsPage class added

--- HubSpot/src/main/java/com/qa/pages/ReportsPage.java
+++ HubSpot/src/main/java/com/qa/pages/ReportsPage.java
@@ -1,5 +1,28 @@
 package com.qa.pages;
 
-public class ReportsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.base.BasePage;
+
+public class ReportsPage extends BasePage{
+	
+	//page objects
+	@FindBy(xpath="//div//h1[@class='private-header__heading']")
+	WebElement reportsLabel;
+	
+	public ReportsPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//page methods
+	public boolean validateReportsLabel() {
+		return reportsLabel.isDisplayed();
+	}
+	
+	public String getReportsPageTitle() {
+		return driver.getTitle();
+	}
 
 }
